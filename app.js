34ab2edb4f28accd1bfab4db697f788d852a9a05
Vivekjaiswal43348 <vivekjaiswal43348@gmail.com>
2024-04-21 17:44:35 +0530

    Using JSX instead of React.createElement() method to render HTML part

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const elem = React.createElement('div',{id: "parent"}, 
-React.createElement('h1', {id: 'child'}, "I am child h1" )            
-);
+/** 
+ * JSX => babel transpiles => React.createElement => React JS - Object => render(JS)
+ * * important information
+ * ! Deprecated method
+ * ? should this method be exposed in the public api
+ * @param the method parameters
+ *  */
+
+const jsxHeading = <h1>I am JSX H1 type heading element</h1>;
+const Heading = () =>{
+    return (
+        <h1>
+            Namste React fuctional component!!
+            {jsxHeading}
+        </h1>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(elem);
+root.render(<Heading />);
