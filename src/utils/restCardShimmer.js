80314e1d7527040af5c4a7d546fb54c35c82bb32
Vivekjaiswal43348 <vivekjaiswal43348@gmail.com>
2024-05-19 17:21:04 +0530

    user defined shimmer UI for loading skeleton

---
+++ src/utils/restCardShimmer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const RestCardShimmer = () => {
+	return (
+		<div className="shimmer-container">
+			<div className="res-shimmer-card"></div>
+			<div className="res-shimmer-card"></div>
+			<div className="res-shimmer-card"></div>
+			<div className="res-shimmer-card"></div>
+			<div className="res-shimmer-card"></div>
+			<div className="res-shimmer-card"></div>
+			<div className="res-shimmer-card"></div>
+			<div className="res-shimmer-card"></div>
+			<div className="res-shimmer-card"></div>
+			<div className="res-shimmer-card"></div>
+			<div className="res-shimmer-card"></div>
+			<div className="res-shimmer-card"></div>
+		</div>
+	);
+};
+
+export default RestCardShimmer;
