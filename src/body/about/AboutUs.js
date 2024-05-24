e32ac9861c999334ace69a53571c972acf6e5567
Vivekjaiswal43348 <vivekjaiswal43348@gmail.com>
2024-05-24 18:51:50 +0530

    git hub API added to display user details

--- src/body/about/AboutUs.js
+++ src/body/about/AboutUs.js
@@ -1,19 +1,35 @@
 import React from "react";
 import AboutClassCard from "./AboutClassCard.JS";
-// import AboutFuncCard from "./AboutFuncCard";
+
 class AboutUs extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			counter: 0,
+			userInfo: {
+				name: "User Name",
+				location: "",
+				email: "email@domain.com",
+				bio: "",
+				avatar_url: null,
+			},
 		};
 		console.log("constructor: parent");
 	}
 	componentWillUnmount() {
 		console.log("componentWillUnmount: : parent");
 	}
-	componentDidMount() {
+	async componentDidMount() {
 		console.log("componentDidMount: : parent");
+		// this.setState({userInfo: {name: "name-1"}});
+
+		const rawResult = await fetch(
+			"https://api.github.com/users/Vivekjaiswal43348"
+		);
+		const jsonResult = await rawResult.json();
+
+		console.log("RESPONSE:", jsonResult);
+		this.setState({ userInfo: jsonResult });
 	}
 	componentDidUpdate() {
 		console.log("componentDidUpdate: : parent");
@@ -31,7 +47,13 @@ class AboutUs extends React.Component {
 					Add
 				</button>
 				<div>
-					<AboutClassCard name={"Vivek Jaiswal"} />
+					<AboutClassCard
+						name={this.state.userInfo.name}
+						location={this.state.userInfo.location}
+						avatar={this.state.userInfo.avatar_url}
+						desc={this.state.userInfo.bio}
+					/>
+					{/* <AboutClassCard name={"Name-2"} /> */}
 				</div>
 			</>
 		);
