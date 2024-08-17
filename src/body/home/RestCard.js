edf650f314fb244175d494cd2d3fac089fc5d7d6
Vivekjaiswal43348 <vivekjaiswal43348@gmail.com>
2024-08-17 16:52:06 +0530

    code refactoring

--- src/body/home/RestCard.js
+++ src/body/home/RestCard.js
@@ -1,16 +1,21 @@
 import { useNavigate } from "react-router-dom";
 
-
 const RestCard = (props) => {
 	/**destructuring the props to get the individual variables */
-	const { image, name, type, rating, totalRatings,  deliveryTime,itemID} = props;
+	const { image, name, type, rating, totalRatings, deliveryTime, itemID } =
+		props;
 	const navigate = useNavigate();
+
+	const cardClickHandler = () => {
+		navigate("/restaurant/" + itemID);
+	};
 	return (
-		<div className="rest-card" onClick={()=>{
-			navigate('/restaurant/'+ itemID)
-			}}>
+		<div className="rest-card" onClick={cardClickHandler}>
 			<div className="food-img">
-				<img alt="food" src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${image}`} />
+				<img
+					alt="food"
+					src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${image}`}
+				/>
 			</div>
 			<div className="food-description">
 				<h3>{name}</h3>
