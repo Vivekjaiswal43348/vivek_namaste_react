899b48322025876bebd4fe6fa4b147375a69cbc1
Vivekjaiswal43348 <vivekjaiswal43348@gmail.com>
2024-08-18 10:39:25 +0530

    commented code removed

--- src/utils/useUserIsOnline.js
+++ src/utils/useUserIsOnline.js
@@ -1,16 +1,13 @@
 import { useEffect, useState } from "react";
 
 const useUserIsOnline = () => {
-	// let isOnline;
 	const [isOnline, setIsOnline] = useState();
 
 	useEffect(() => {
 		window.addEventListener("online", () => {
-			// isOnline = true;
 			setIsOnline(true);
 		});
 		window.addEventListener("offline", () => {
-			// isOnline = false;
 			setIsOnline(false);
 		});
 	}, []);
