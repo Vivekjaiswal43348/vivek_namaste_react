87b1e5e58521335eb1617f55de202602d54b4b96
Vivekjaiswal43348 <vivekjaiswal43348@gmail.com>
2024-08-24 19:41:01 +0530

    responsive shimmer UI added

--- src/utils/restCardShimmer.js
+++ src/utils/restCardShimmer.js
@@ -2,19 +2,19 @@ import React from "react";
 
 const RestCardShimmer = () => {
 	return (
-		<div className="shimmer-container">
-			<div className="res-shimmer-card"></div>
-			<div className="res-shimmer-card"></div>
-			<div className="res-shimmer-card"></div>
-			<div className="res-shimmer-card"></div>
-			<div className="res-shimmer-card"></div>
-			<div className="res-shimmer-card"></div>
-			<div className="res-shimmer-card"></div>
-			<div className="res-shimmer-card"></div>
-			<div className="res-shimmer-card"></div>
-			<div className="res-shimmer-card"></div>
-			<div className="res-shimmer-card"></div>
-			<div className="res-shimmer-card"></div>
+		<div className="flex flex-wrap justify-center sm:justify-start">
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
+			<div className="w-40 sm:w-52 h-72 bg-gray-300 m-1"></div>
 		</div>
 	);
 };
