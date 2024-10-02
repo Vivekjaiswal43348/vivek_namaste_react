325ad728349cb919c0aa2cec5602deaaedd65d9f
Vivekjaiswal43348 <vivekjaiswal43348@gmail.com>
2024-10-02 16:26:14 +0530

    made the header logo clickable and will navigate to home page

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,19 +2,25 @@ import Logo from "../../public/images/foodAppLogo.jpeg";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useUserIsOnline from "../utils/useUserIsOnline";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const navigate  = useNavigate();
 	let isOnline = useUserIsOnline();
-
+	goToHomePage = () => {
+		navigate("/");
+	};
 	return (
 		<div className="flex flex-col justify-between p-5 bg-gray-100 items-center sm:flex-row">
-			<div className="w-[100px] md:w-24 hidden sm:block">
+			<div title="Go To home page" className="w-[100px] md:w-24 hidden sm:block cursor-pointer" onClick={goToHomePage}>
 				<img alt="app-logo" src={Logo} />
 			</div>
 			<div className="">
 				<ul className="flex gap-x-[10px] sm:gap-x-6 text-nowrap flex-wrap text-sm sm:text-base">
-					<li title={`You are ${isOnline === false ? "Offline 😒" : "Online 😍"}`} className="hover:cursor-pointer">
+					<li
+						title={`You are ${isOnline === false ? "Offline 😒" : "Online 😍"}`}
+						className="hover:cursor-pointer">
 						{`🛜 ${isOnline === false ? "❌" : "✅"}`}
 					</li>
 					<li>
@@ -29,8 +35,10 @@ const Header = () => {
 					<li>Cart</li>
 					<li className="login-bth">
 						<button
-							// style={{ backgroundColor: isLoggedIn ? "#61a461" : "#dc6c6c" }}	
-							className={`${!isLoggedIn? 'bg-red-300' : 'bg-green-300'} p-1 text-sm rounded-sm w-[54px] `}
+							// style={{ backgroundColor: isLoggedIn ? "#61a461" : "#dc6c6c" }}
+							className={`${
+								!isLoggedIn ? "bg-red-300" : "bg-green-300"
+							} p-1 text-sm rounded-sm w-[54px] `}
 							onClick={() => {
 								setIsLoggedIn(!isLoggedIn);
 							}}>
