2561d17e0392c47b484e01c693ae2cdd215f9af0
Vivekjaiswal43348 <vivekjaiswal43348@gmail.com>
2024-10-02 16:26:40 +0530

    added accordion to detail page

---
+++ src/body/home/CardAccordion.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { useState } from "react";
+import FOOD_IMG_PLACEHOLDER from "./../../../public/images/food_img_placeholder.png";
+
+const CardAccordion = (data) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const itemCards = data.data.itemCards;
+
+	return (
+		<div className={`border border-gray-200 p-[3] mb-1 rounded-md bg-gray-200 cursor-pointer ${!isOpen && 'hover:border-red-400'}`}>
+			<div className={`flex justify-between p-[6] rounded-sm ${isOpen && 'bg-zinc-300'}`} onClick={() => setIsOpen(!isOpen)}>
+				<p className={isOpen ? "font-bold underline" : "font-bold"}>{data.data.title}</p>
+				{/* DOWN Icon */}
+				{!isOpen && (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 16 16"
+						fill="currentColor"
+						class="size-4">
+						<path
+							fill-rule="evenodd"
+							d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				)}
+				{/* UP Icon */}
+				{isOpen && (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 16 16"
+						fill="currentColor"
+						class="size-4">
+						<path
+							fill-rule="evenodd"
+							d="M11.78 9.78a.75.75 0 0 1-1.06 0L8 7.06 5.28 9.78a.75.75 0 0 1-1.06-1.06l3.25-3.25a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				)}
+			</div>
+			{isOpen && (
+				<div className="">
+					<ul style={{ listStyle: "none" }}>
+						{itemCards ? (
+							itemCards.map((item) => {
+								return (
+									<li key={item.card.info.id} className="m-2">
+										<div className="flex justify-between">
+											<div className="left-section w-[70%]">
+												<p className="font-medium">{item.card.info.name}</p>
+												<p className="text-sm">{`Rs.${
+													item.card.info.price / 100
+												}`}</p>
+												{item.card.info.ratings.aggregatedRating.rating && (
+													<p>
+														<span className="text-sm">
+															{`*${item.card.info.ratings.aggregatedRating.rating}`}
+														</span>
+														<span className="">
+															{` (${item.card.info.ratings.aggregatedRating.ratingCountV2})`}
+														</span>
+													</p>
+												)}
+												<p className="text-xs">{item.card.info.description}</p>
+											</div>
+											<div className=" p-2">
+												<img
+													className="rounded-sm w-[100] h-[100] object-cover"
+													alt="item-img"
+													src={
+														item.card.info.imageId
+															? "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/" +
+															  item.card.info.imageId
+															: FOOD_IMG_PLACEHOLDER
+													}
+												/>
+											</div>
+										</div>
+										<hr></hr>
+									</li>
+								);
+							})
+						) : (
+							<>
+								<h5 className="text-center	font-bold text-gray-400">
+									No Data Found
+								</h5>
+							</>
+						)}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default CardAccordion;
--- src/body/home/RestaurantMenu.js
+++ src/body/home/RestaurantMenu.js
@@ -1,8 +1,8 @@
 import RestCardShimmer from "../../utils/restCardShimmer";
 import { useParams } from "react-router-dom";
 import "./RestaurantMenu.css";
-import FOOD_IMG_PLACEHOLDER from "./../../../public/images/food_img_placeholder.png";
 import useRestaurantMenu from "../../utils/useRestaurantMenu";
+import CardAccordion from "./CardAccordion";
 
 const RestaurantMenu = () => {
 	const { resID } = useParams();
@@ -17,72 +17,41 @@ const RestaurantMenu = () => {
 	);
 	const { name, costForTwoMessage, avgRatingString, totalRatingsString } =
 		resMenuData?.data?.cards[2]?.card.card.info;
-	const itemCards =
-		resMenuData?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]
-			?.card?.card?.itemCards;
+
+	const accordionData =
+		resMenuData?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+			(item) => {
+				return (
+					item.card.card["@type"] ===
+					"type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+				);
+			}
+		);
+
+
 	return (
-		<div className="mx-32 my-6">
+		<div className="mx-60 my-6">
 			<div className="flex justify-between items-center">
-			<div className="my-3">
-				<h2 className="text-lg font-bold underline">{name}</h2>
-				<h4 className="text-sm">{`${avgRatingString} (${totalRatingsString}) . ${costForTwoMessage}`}</h4>
+				<div className="my-3">
+					<h2 className="text-lg font-bold underline">{name}</h2>
+					<h4 className="text-sm">{`${avgRatingString} (${totalRatingsString}) . ${costForTwoMessage}`}</h4>
+				</div>
+				<button
+					onClick={() => {
+						history.back();
+					}}
+					className="w-24 bg-sky-100 hover:shadow-md">
+					⬅️Go Back
+				</button>
 			</div>
-			<button
-				onClick={() => {
-					history.back();
-				}}
-				className="w-24 bg-sky-100 hover:shadow-md">
-				⬅️Go Back
-			</button>
-			</div>
-			<div className="">
-				{itemCards && 
-				<h2 className="text-lg font-medium underline">Recommended ({itemCards.length})</h2>}
-				<ul style={{ listStyle: "none" }}>
-					{itemCards ? (
-						itemCards.map((item) => {
-							return (
-								<li key={item.card.info.id} className="m-8">
-									<div className="flex justify-between">
-										<div className="left-section">
-											<p className="font-medium">{item.card.info.name}</p>
-											<p className="text-sm">{`Rs.${
-												item.card.info.price / 100
-											}`}</p>
-											{item.card.info.ratings.aggregatedRating.rating && <p>
-												<span className="text-sm">
-													{`*${item.card.info.ratings.aggregatedRating.rating}`}
-												</span>
-												<span className="">
-													{` (${item.card.info.ratings.aggregatedRating.ratingCountV2})`}
-												</span>
-											</p>}
-											<p className="text-xs">{item.card.info.description}</p>
-										</div>
-										<div className="w-24 p-2">
-											<img
-											className="rounded-sm"
-												alt="item-img"
-												src={
-													item.card.info.imageId
-														? "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/" +
-														  item.card.info.imageId
-														: FOOD_IMG_PLACEHOLDER
-												}
-											/>
-										</div>
-									</div>
-									<hr></hr>
-								</li>
-							);
-						})
-					) : (
-						<>
-							<h5 className="text-center	font-bold text-gray-400">No Data Found</h5>
-						</>
-					)}
-				</ul>
-			</div>
+
+			{accordionData.map((item, index) => (
+				<>
+				<CardAccordion 
+				data={item.card.card} 
+				key={index} />
+				</>
+			))}
 		</div>
 	);
 };
